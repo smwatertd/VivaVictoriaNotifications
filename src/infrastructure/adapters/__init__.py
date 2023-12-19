b6536d8b136ea148b673b8f7800b91e69641448c
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-19 22:25:57 +0500

    add MessageType

---
+++ src/infrastructure/adapters/__init__.py
@@ -0,0 +1,4 @@
+from infrastructure.adapters.consumers import RabbitMQConsumer
+from infrastructure.adapters.message_types import MessageType
+from infrastructure.adapters.messages import Message
+from infrastructure.adapters.producers import RedisProducer
