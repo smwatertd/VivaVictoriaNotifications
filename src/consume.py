32f71835f23a7a081fa9163a3e9fa5d0b5092b55
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-21 23:12:42 +0500

    change str to settings in listen method call

--- src/consume.py
+++ src/consume.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 
 from core.container import container
+from core.settings import message_broker_settings
 
 
 BASE_DIR = Path(__file__).parent
@@ -14,10 +15,11 @@ logger.addHandler(logging.FileHandler(BASE_DIR / 'consume.log', encoding='utf-8'
 async def main() -> None:
     message_consumer = container.message_consumer()
     game_message_producer = container.game_message_producer()
-    async for message in message_consumer.listen('notifications.games.events'):
+    async for message in message_consumer.listen(message_broker_settings.game_events_queue):
         try:
             game_id = message.payload.pop('game_id')
             await game_message_producer.publish(game_id, message)
+            await message_consumer.commit()
         except KeyError:
             logger.error(f'Invalid message: {message}')
 
